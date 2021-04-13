e27521fa2b06388ec9e083778bc47efd41f79b7d
diegun_99 <diegun123456789@gmail.com>
2021-04-13 17:48:57 -0500

    conexion a base de datos lograda

---
+++ server/src/keys.ts
@@ -0,0 +1,8 @@
+export default {
+    database :  { // datos para conectarse a la base de datos
+        host : 'localhost',
+        user : 'root',
+        password : '',
+        database : 'ng_games_db',
+    }
+}
