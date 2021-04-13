14169d445ad794dae4081615bbd9a502581680d4
diegun_99 <diegun123456789@gmail.com>
2021-04-12 21:29:12 -0500

    configuracion basica servidor

--- server/build/index.js
+++ server/build/index.js
@@ -1,1 +1,22 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+class Server {
+    constructor() {
+        this.app = express_1.default();
+    }
+    config() {
+        this.app.set('PORT', process.env.PORT || 3000);
+    }
+    routes() {
+    }
+    start() {
+        this.app.listen(this.app.get('PORT'), () => {
+            console.log('server on port 3000');
+        });
+    }
+}
+new Server();
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,31 @@
-console.log('IT WORKS');
+
+import express, {Application} from 'express'
+
+class Server {
+    public app : Application;
+    constructor(){
+        this.app = express();
+
+    }
+
+    config() : void {
+        this.app.set('PORT',process.env.PORT || 3000);
+
+    }
+
+    routes() : void { // para definir las rutas http 
+
+    }
+
+    start(): void {// inicializar el servidor
+        this.app.listen(this.app.get('PORT'), ()=> {
+            console.log('server on port 3000')
+
+        });
+
+
+    }
+
+}
+
+new Server ();
